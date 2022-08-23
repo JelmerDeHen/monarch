fe157fa00bfab636b449917ea448fbaa34d3f5fd
J <18084450+JelmerDeHen@users.noreply.github.com>
2022-08-23 16:54:16 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-systemctl --user restart monarch_arecord
-systemctl --user restart monarch_v4l2
-systemctl --user restart monarch_x11grab
+function start() {
+  systemctl --user restart monarch_arecord
+  systemctl --user restart monarch_v4l2
+  systemctl --user restart monarch_x11grab
+}
+
+function stop() {
+  systemctl --user stop monarch_arecord
+  systemctl --user stop monarch_v4l2
+  systemctl --user stop monarch_x11grab
+}
+
+function deploy() {
+  cp -v sd/* "${HOME}/.config/systemd/user/"
+  systemctl --user daemon-reload
+}
+
